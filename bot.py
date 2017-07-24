ab95bda1255b9c5cff7e5e63fb7b7f5546bbff64
hibald <hibald.etc@gmail.com>
2017-07-25 01:07:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет, {name}. Рад тебя видеть.'.format(name=message.chat.id))
+    bot.send_message(message.chat.id, 'Привет, рад тебя видеть.'.format(name=message.chat.id))
 
 
 
