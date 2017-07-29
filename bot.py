698f06c4b8aa90a446ba1e229c9aa558a67a53f8
Pavel Sutyrin <pavel.sutyrin@gmail.com>
2017-07-29 17:17:19 +0300

    updated bot

--- bot.py
+++ bot.py
@@ -1,14 +1,11 @@
+# -*- coding: utf
 import telebot # pip install pyTelegramBotAPI
 
-
-TOKEN = ''
+TOKEN = 'ТОКЕН'  # заменить на настоящий токен от BotFather
 bot = telebot.TeleBot(TOKEN)
-
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Привет, рад тебя видеть.'.format(name=message.chat.id))
-
-
 
 bot.polling()
