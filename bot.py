d2f2156c45e5a005833657aae4251744944e68db
Pavel Sutyrin <pavel@sutyrin.pro>
2017-07-29 14:32:47 +0000

    tweaked

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
 # -*- coding: utf
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
 import telebot # pip install pyTelegramBotAPI
 
-TOKEN = 'ТОКЕН'  # заменить на настоящий токен от BotFather
+TOKEN = '-- INSERT TOKEN HERE --'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет, рад тебя видеть.'.format(name=message.chat.id))
+    print('< [{0}] "{1}"'.format(message.chat.username, message.text))
+    
+    msg = 'Привет, {0}, рад тебя видеть!'.format(message.chat.username)
+    
+    print('> [{0}] "{1}"'.format(message.chat.username, msg))
+    bot.send_message(message.chat.id, msg)
 
+print('bot started')
 bot.polling()
--- setup.sh
+++ setup.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install python3.5 python3.5-dev python3.5-venv -y
-python3.5 -m venv env
-. env/bin/activate
-pip install -r requiments.txt
+sudo pip install -r requiments.txt
